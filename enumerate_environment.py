129535225e4d811bf314bbd71cc9335c5a9f26d3
William Deegan <bill@baddogconsulting.com>
2018-01-16 19:08:21 -0500

    List directories under c:\

--- enumerate_environment.py
+++ enumerate_environment.py
@@ -1,5 +1,16 @@
+from __future__ import print_function
 import os
+
+def check_dir(path):
+    print(r'-- Directories under %s'%path)
+    for p in os.listdir(path):
+        print("Path->%s"%p)
+
 
 pathdirs = os.environ['PATH'].split(os.pathsep)
 for p in pathdirs:
     print("Path->%s"%p)
+
+check_dir('c:\\')
+check_dir('C:/Program Files (x86)/')
+check_dir('C:/Program Files/')
