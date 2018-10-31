b970119286dd7dc92dcac9e42990274faa54c6cd
William Deegan <bill@baddogconsulting.com>
2018-10-30 19:10:14 -0700

    see if we can locate xsltproc

--- enumerate_environment.py
+++ enumerate_environment.py
@@ -3,6 +3,7 @@ Figure out paths in appveyor environment
 """
 from __future__ import print_function
 import os
+import os.path
 
 def check_dir(path):
     """ enumerate files/directories under path"""
@@ -15,6 +16,11 @@ PATHDIRS = os.environ['PATH'].split(os.pathsep)
 for p in PATHDIRS:
     print("Path->%s"%p)
 
+def find_file(filename):
+    for p in PATHDIRS:
+        if os.path.exists(os.path.join(p,filename)):
+            print("Found %s in %s"%(filename, p))
+
 check_dir('c:\\')
 check_dir('C:/Program Files (x86)/')
 check_dir('C:/Program Files/')
@@ -24,3 +30,5 @@ check_dir('C:/Program Files/LLVM/bin')
 check_dir('C:/msys64')
 
 check_dir('C:/ProgramData/chocolatey/bin')
+
+find_file('xsltproc.exe')
